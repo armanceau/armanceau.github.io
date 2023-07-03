8045aa31b5210b0529d3cce82ec7fa1cff6abd74
armanceau <100920811+armanceau@users.noreply.github.com>
2023-07-03 12:32:51 +0200

    add
    ajout de json + js qui va le chercher + design de la premiere page "cv about me"

--- mobile/script.js
+++ mobile/script.js
@@ -44,6 +44,7 @@ const tags = document.querySelectorAll('.tag');
 const btnBurger = document.querySelector('.menu__btn');
 const menuBurger = document.querySelector('.menu__box');
 const right = document.querySelector('.right');
+const mainSkill = document.querySelector('#main-skills');
 const detailProjects = document.querySelectorAll('.detailProject');
 
 switchBox.addEventListener('click', function(){
@@ -54,6 +55,7 @@ switchBox.addEventListener('click', function(){
   title1?.classList.toggle('h1-change');
   title2?.classList.toggle('h2-change');
   right?.classList.toggle('right-change');
+  mainSkill?.classList.toggle('main-skills-change');
   btnBurger?.classList.toggle('menu__btn-change');
 
   detailProjects.forEach(detailProject => {
@@ -79,6 +81,7 @@ addEventListener("DOMContentLoaded", (event) => {
     title1?.classList.toggle('h1-change');
     title2?.classList.toggle('h2-change'); 
     right?.classList.toggle('right-change');
+    mainSkill?.classList.toggle('main-skills-change');
     btnBurger?.classList.toggle('menu__btn-change');
 
     detailProjects.forEach(detailProject => {
@@ -94,3 +97,143 @@ addEventListener("DOMContentLoaded", (event) => {
 });
 
 // ################################################################################################
+
+
+var request = new XMLHttpRequest();
+request.open('GET', 'data.json', true);
+
+request.onload = function() {
+    var data = JSON.parse(request.responseText);
+
+    const aboutFilter = document.getElementById("about");
+    const experienceFilter = document.getElementById("experience");
+    const educationFilter = document.getElementById("education");
+    const mainSkillsSection = document.getElementById("main-skills");
+    const mainExperiencesSection = document.getElementById("main-experiences");
+    const mainEducationsSection = document.getElementById("main-educations");
+    const mainInterestsSection = document.getElementById("main-interests");
+
+    // Fonction pour afficher les données de "About Me"
+    function showAbout() {
+        // Récupérer les données de "About Me" du JSON
+        const aboutData = data.aboutMe;
+
+        // Générer le contenu HTML correspondant
+        let aboutHTML = '<div id="main-skills-title">Skills</div>';
+        aboutData.forEach(function(item) {
+            aboutHTML += `
+                <div class="main-skill">
+                    <div class="main-skills-text">
+                        <div class="main-skills-title">${item.libelle}</div>
+                        <div class="main-skills-pourcentage">${item.pourcentage}%</div>
+                    </div>
+    
+                    <div class="main-skills-lines">
+                        <div class="main-skills-line"></div>
+                        <div class="main-skills-line-color"></div>
+                    </div>
+                </div>
+                
+            `;
+        });
+
+        // Afficher le contenu dans la section "main-skills"
+        mainSkillsSection.innerHTML = aboutHTML;
+        mainExperiencesSection.innerHTML = "";
+        mainEducationsSection.innerHTML = "";
+
+        // Ajouter la classe "filter-active" à "About me"
+        aboutFilter.classList.add('filter-active');
+        experienceFilter.classList.remove('filter-active');
+        educationFilter.classList.remove('filter-active');
+
+        // mainInterestsSection.style.display = "block";
+
+        // Sélectionner tous les éléments avec la classe ".main-skills-line-color"
+        const mainSkillsLines = document.querySelectorAll(".main-skills-line-color");
+
+        // Itérer sur chaque élément et mettre à jour la propriété CSS "width" en fonction du pourcentage
+        aboutData.forEach(function(item, index) {
+        const widthPercentage = item.pourcentage + "%";
+        mainSkillsLines[index].style.width = widthPercentage;
+        });
+    }
+
+    // Fonction pour afficher les données de "Experience"
+    function showExperience() {
+        // Récupérer les données de "Experience" du JSON
+        const experienceData = data.experience;
+
+        let experienceHTML = '';
+        experienceData.forEach(function(item) {
+            experienceHTML += `
+                <div class="main-experience">
+                    <div class="main-experience-img">
+                        <img src="assets/icons/experience.png" alt="Experience image" height="30" width="30">
+                    </div>
+                    <div class="main-experience-text">
+                        <div class="main-experience-text-title">${item.libelle}</div>
+                        <div class="main-experience-text-text">${item.text}</div>
+                    </div>
+                </div>
+            `;
+        });
+
+        // Afficher le contenu dans la section "main-skills"
+        mainExperiencesSection.innerHTML = experienceHTML;
+        mainSkillsSection.innerHTML = "";
+        mainEducationsSection.innerHTML = "";
+
+        // Ajouter la classe "filter-active" à "Experience"
+        aboutFilter.classList.remove('filter-active');
+        experienceFilter.classList.add('filter-active');
+        educationFilter.classList.remove('filter-active');
+
+        // mainInterestsSection.style.display = "none";
+        mainSkillsTitle.style.display = "none";
+    }
+
+    // Fonction pour afficher les données de "Education"
+    function showEducation() {
+        // Récupérer les données de "Education" du JSON
+        const educationData = data.education;
+
+        let educationHTML = '';
+        educationData.forEach(function(item) {
+            educationHTML += `
+                <div class="main-education">
+                    <div class="main-education-img">
+                        <img src="assets/icons/education.png" alt="Experience image" height="30" width="30">
+                    </div>
+                    <div class="main-education-text">
+                        <div class="main-education-text-title">${item.libelle}</div>
+                        <div class="main-education-text-text">${item.text}</div>
+                    </div>
+                </div>
+            `;
+        });
+
+        mainEducationsSection.innerHTML = educationHTML;
+        mainExperiencesSection.innerHTML = "";
+        mainSkillsSection.innerHTML = "";
+
+        // Ajouter la classe "filter-active" à "Education"
+        aboutFilter.classList.remove('filter-active');
+        experienceFilter.classList.remove('filter-active');
+        educationFilter.classList.add('filter-active');
+
+        // mainInterestsSection.style.display = "none";
+    }
+
+    // Écouter les événements de clic sur les filtres
+    aboutFilter.addEventListener('click', showAbout);
+    experienceFilter.addEventListener('click', showExperience);
+    educationFilter.addEventListener('click', showEducation);
+
+    // Afficher par défaut les données de "About Me"
+    showAbout();
+    
+};
+
+request.send();
+
